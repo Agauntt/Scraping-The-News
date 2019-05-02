3906a587468792327d692c62fbae6ee1d23ae2b9
Aaron <gauntta@gmail.com>
2019-05-02 13:01:57 -0500

    Comment system in progress

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i].id + "'>" + data[i].title + "<br />" + "<a href='" + data[i].link + "'>" +  data[i].link  + "</p>");
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + "<a href='" + data[i].link + "'>" +  data[i].link  + "</p>");
     }
   });
 
@@ -17,5 +17,20 @@ $.getJSON("/articles", function(data) {
   }).onsubmit="setTimeout(function(){window.location.reload();},10)";
 
   $(document).on("click", "p", function() {
+    var thisID = $(this).attr("data-id");
     console.log("p tag clicked");
+    console.log(thisID);
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisID
+    })
+      .then(function(data) {
+        console.log(data);
+      })
+    $("#comment-modal").modal("show");
+    $(".modal-footer").prepend('<button type="button" data-id="' + thisID + ' id="comment-submit" class="btn btn-primary">Save Comment</button>');
   });
+
+  $("#comment-submit").click(function() {
+
+  })
