cf5c5e8ccb50fb2b529c88b95a82aa28f83ae01e
Aaron <gauntta@gmail.com>
2019-05-01 22:26:37 -0500

    Full local functionality

--- public/app.js
+++ public/app.js
@@ -6,3 +6,16 @@ $.getJSON("/articles", function(data) {
       $("#articles").append("<p data-id='" + data[i].id + "'>" + data[i].title + "<br />" + "<a href='" + data[i].link + "'>" +  data[i].link  + "</p>");
     }
   });
+
+  $("#scrapper").click(function() {
+      // Send the POST request.
+      $.ajax("/scrape", {
+        type: "GET"
+    }).then(function() {
+        console.log("new scrape");
+    });
+  }).onsubmit="setTimeout(function(){window.location.reload();},10)";
+
+  $(document).on("click", "p", function() {
+    console.log("p tag clicked");
+  });
--- server.js
+++ server.js
@@ -26,48 +26,53 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
+// Drops the database,
+// mongoose.connect("mongodb://localhost/scrapednews")
 mongoose.connect("mongodb://localhost/scrapednews", { useNewUrlParser: true });
 
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://roguerocket.com/category/us/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-    
-
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("li.mvp-blog-story-wrap").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .find(".mvp-blog-story-text")
-        .children("h2")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
-    });
-
-    // Send a message to the client
-    res.send("Scrape Complete");
+  // First, we clear the current Articles in the db, so only the newest will be displayed
+  db.Article.remove({}, function(err) { 
+    console.log('collection removed') 
   });
+  // Then, we grab the body of the html with axios
+  axios
+    .get("https://roguerocket.com/category/us/")
+    .then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("li.mvp-blog-story-wrap").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .find(".mvp-blog-story-text")
+          .children("h2")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+          });
+      });
+
+      // Send a message to the client
+      res.send("Scrape Complete");
+    });
 });
 
 // Route for getting all Articles from the db
@@ -108,7 +113,11 @@ app.post("/articles/:id", function(req, res) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
     })
     .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
